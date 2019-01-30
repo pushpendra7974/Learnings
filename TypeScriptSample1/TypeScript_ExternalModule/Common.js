1729c51e4ee654d58783b37eea6603f37b03c0a7
pushpendra7974 <pushpendra7974@hotmail.com>
2019-01-30 17:05:28 +0530

    External Module Js files inserted

---
+++ TypeScriptSample1/TypeScript_ExternalModule/Common.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var common;
+(function (common) {
+    var Login;
+    (function (Login) {
+        var User = /** @class */ (function () {
+            function User() {
+            }
+            return User;
+        }());
+        Login.User = User;
+    })(Login = common.Login || (common.Login = {}));
+})(common = exports.common || (exports.common = {}));
---
+++ TypeScriptSample1/TypeScript_ExternalModule/Second.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Common_1 = require("./Common");
+var obj = new Common_1.common.Login.User();
+// And here is another way
+/*
+    import {common as C} from "./Common"
+    var obj = new C.Login.User();
+*/ 
